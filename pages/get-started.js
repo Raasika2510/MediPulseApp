e46aedd7c760f6a3f30ccfdb3077c3f634b72623
Megha Manoj <manojmegha2002@gmail.com>
2024-08-10 15:37:08 +0400

    Update get-started.js

--- pages/get-started.js
+++ pages/get-started.js
@@ -10,7 +10,7 @@ export default function GetStarted() {
 
   const {register, handleSubmit, reset, formState:{errors}} = useForm()
  
-   data.CLIENT_EMAIL = "ai-support@join-waitlist-431213.iam.gserviceaccount.com"
+   data.client_email= "ai-support@join-waitlist-431213.iam.gserviceaccount.com"
 
     function submitHandler(data){
     console.log(data)
