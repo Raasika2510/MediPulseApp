e2535b8c55f4a3e91dc704ec31813cd85729a69c
Megha Manoj <manojmegha2002@gmail.com>
2024-08-10 15:46:43 +0400

    Update sheet.js

--- pages/api/sheet.js
+++ pages/api/sheet.js
@@ -3,8 +3,13 @@ async function  handler(req, res) {
     if (req.method === 'POST'){
         const {Name, Email, Concern} = req.body
         console.log(Name, Email, Concern)
-        const auth= new google.auth.GoogleAuth({
-             keyFile: 'secrets.json',
+         const auth= new google.auth.GoogleAuth({
+            projectId: "join-waitlist-431213",
+        credentials:{
+            client_email: process.env.CLIENT_EMAIL,
+            client_id: process.env.CLIENT_ID,
+            private_key : process.env.PRIVATE_KEY.replace(/\\n/g, "\n")
+        },
         scopes:[
             'https://www.googleapis.com/auth/drive',
               'https://www.googleapis.com/auth/drive.file',
