d71d56fadbc5268a14edb9270d7226bf77e36c6d
Raasika Mageshwaran <121993730+Raasika2510@users.noreply.github.com>
2024-08-11 11:23:22 +0530

    Update route.js

--- pages/api/route.js
+++ pages/api/route.js
@@ -35,9 +35,9 @@ When the user shows gratitude, respond with: "I’m glad I could assist. Wishing
 `;
 
 // POST function to handle incoming requests
-async function POST(req) {
+export async function POST(req) {
   // create a new instance of the GoogleGenerativeAI client
-  const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_API_KEY);
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
   //parse the json body from the request
@@ -70,5 +70,3 @@ async function POST(req) {
 
   return new NextResponse(stream); // Return the stream as the response
 }
- 
-export default POST;
